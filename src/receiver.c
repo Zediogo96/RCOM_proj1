9b4448eea553be4927577055cbfb0677518d1003
Zé Diogo <zediogox@hotmail.com>
2022-10-14 09:58:18 +0100

    receiver.c helper function created

--- src/receiver.c
+++ src/receiver.c
@@ -5,3 +5,19 @@
 #include "receiver.h"
 
 unsigned char buffer[BUF_SIZE] = {0};
+
+int receiverStart(int fd)
+{
+    while (TRUE)
+    {
+        int _bytes = read(fd, buffer, 1);
+        if (buf != 0 && _bytes > -1)
+        {
+            int ans = startVerifyState(buffer[0], fd, L1Rx);
+            if (ans == 1)
+                return 1; // POSSÍVEL REFACTOR AQUI
+        }
+    }
+
+    return 0;
+}
