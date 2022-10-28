a75e58a5a1af5312cd15b6a25e04368e98c588c1
Zé Diogo <zediogox@hotmail.com>
2022-10-28 04:25:39 +0100

    added comments to packet.c

--- src/packet.c
+++ src/packet.c
@@ -12,6 +12,7 @@ unsigned int get_controlpacket(unsigned char *filename, int start, unsigned char
 {
     int size_packet = 0;
 
+    // CHECK IF FILENAME DOESN'T FIT INTO ONE BYTE
     if (strlen(filename) > 255)
     {
         printf("Size of filename couldn't fit in one byte: %d\n, 2");
@@ -23,7 +24,7 @@ unsigned int get_controlpacket(unsigned char *filename, int start, unsigned char
     struct stat file;
     stat(filename, &file);
 
-    // Reads string directly as hexadecimal
+    // READS STRING IN HEXADECIMAL
     sprintf(hex_string, "%02x", file.st_size);
 
     int idx = 3;
@@ -32,40 +33,39 @@ unsigned int get_controlpacket(unsigned char *filename, int start, unsigned char
 
     printf("\n File Size: %d\n", file_size);
 
+    // CHECK IF FILESIZE NUMBER DOESN'T FIT INTO ONE BYTE
     if (file_size_bytes > 256)
     {
         printf("size of file couldn't fit into 1 Byte\n");
     }
 
-
     printf("packet size in Bytes: %02x\n", file_size_bytes);
 
+    // APPENDS CONTROL DEPENDING IF IT'S THE START CONTROL PACKET OR END ON
     (start == TRUE) ? (packet[0] = C_START) : (packet[0] = C_END);
-    /* if (start == TRUE)
-        packet[index++] = C_START;
-    else if (start == FALSE)
-        packet[index++] = C_END; */
 
     // APPENDING T_SIZE TO PACKET
     packet[1] = T_SIZE;
     packet[2] = file_size_bytes;
-    for (int i= file_size_bytes-1, j = 0; i>-1; i--, j++) {
-        packet[3+j]=file_size >> (8*i);
-    
+    for (int i = file_size_bytes - 1, j = 0; i > -1; i--, j++)
+    {
+        packet[3 + j] = file_size >> (8 * i);
     }
     // NOT SURE IF THE ORDER T_NAME 1ST & T_SIZE 2ND OR VICE-VERSA MATTERS
-    packet[idx+file_size_bytes] = T_NAME;
-    packet[idx+file_size_bytes+1] = strlen(filename);
+    packet[idx + file_size_bytes] = T_NAME;
+    packet[idx + file_size_bytes + 1] = strlen(filename);
 
+    // APPENDS FILENAME BYTES INTO THE PACKET
     for (int i = 0; i < strlen(filename); i++)
     {
-        packet[idx+2+file_size_bytes+i] = filename[i];
+        packet[idx + 2 + file_size_bytes + i] = filename[i];
     }
 
-    size_packet = 5+file_size_bytes+strlen(filename);
+    size_packet = 5 + file_size_bytes + strlen(filename);
 
-    for (int i = 0; i < size_packet; i++) {
-        printf("%02x ",packet[i]);
+    for (int i = 0; i < size_packet; i++)
+    {
+        printf("%02x ", packet[i]);
     }
 
     printf("\n");
@@ -73,19 +73,24 @@ unsigned int get_controlpacket(unsigned char *filename, int start, unsigned char
 }
 
 unsigned int get_datapacket(unsigned char *bytes, unsigned char *packet, int nSequence, int count_bytes)
-{
+{   
+
+    // CALCULATIONS FOR FIRST PART OF DATA PACKET
     int l1 = div(count_bytes, 256).rem;
     int l2 = div(count_bytes, 256).quot;
 
+    // APPENDS CONTROL, L1, L2 TO DATAPACKET
     packet[0] = 0x01;
     packet[1] = div(nSequence, 255).rem;
     packet[2] = l2;
     packet[3] = l1;
 
+    // APPENDS DATA INTO DATAPACKET
     for (int i = 0; i < count_bytes; i++)
     {
         packet[i + 4] = bytes[i];
     }
 
-    return count_bytes + 4; // for the initial data packet
+    // +4 BECAUSE OF INITIAL CONTROL PACKET
+    return count_bytes + 4; 
 }
